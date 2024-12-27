68395bdf534577e2072c3b13bf2fc14dbfa2f1e9
hffrds <154441320+hffrds@users.noreply.github.com>
2024-12-27 22:02:43 +0800

    Update vector.cpp

--- vector.cpp
+++ vector.cpp
@@ -65,3 +65,14 @@ public:
 
 // 区间和
 保存前缀和的数组然后相减即可（输入数据与计算前缀和可在一个循环）
+
+//栈
+template <class Type, class Container= deque <Type>>
+class stack//栈数据结构用于模拟后进先出的特性
+需要包含头文件<stack>。container为基础容器，vector，list，deque(默认deque)，需要有push_back(push),pop_back(pop),back(top),size(size),empty(empty)
+模拟栈一般用专门的stack，不过string也有pop_back和push_back,可以用来模拟栈的先入后出而无需引入栈头文件
+
+vector.front()访问第一个元素，vector.at()访问指定，vec.data()返回第一个指针(普通指针，不能赋值于迭代器)，vec.insert(position, value)/vec.erase(position)/vec.erase(begin, end)/vec.clear()/vec.size()/vec.capacity()
+vec.resize(new_size)改变大小/vec.reserve(capacity)预分配/vec.swap(other_vector)交换两个vector的内容，实际只交换指针和状态信息/vec.assign(n, value)重置，可以用于初始化/std::find(vec.begin(), vec.end(), value)查找value是否在vector中。如果找到，返回该元素的迭代器，否则返回vec.end()
+vec.begin()/end()返回迭代器
+vec == other_vector
