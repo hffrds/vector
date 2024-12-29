5f265205e443a44174a9764a487514035db323e0
hffrds <154441320+hffrds@users.noreply.github.com>
2024-12-29 21:47:34 +0800

    Update vector.cpp

--- vector.cpp
+++ vector.cpp
@@ -71,9 +71,19 @@ template <class Type, class Container= deque <Type>>
 class stack//栈数据结构用于模拟后进先出的特性
 需要包含头文件<stack>。container为基础容器，vector，list，deque(默认deque)，需要有push_back(push),pop_back(pop),back(top),size(size),empty(empty)
 模拟栈一般用专门的stack，不过string也有pop_back和push_back,可以用来模拟栈的先入后出而无需引入栈头文件 //普通数组没有pop_back与push_back，但可以定义两个变量用于模拟更新尾部与首部，也可以模拟栈
+stack2 = stack1
 
 vector.front()访问第一个元素，vector.at()访问指定，vec.data()返回第一个指针(普通指针，不能赋值于迭代器)，vec.insert(position, value)/vec.erase(position)/vec.erase(begin, end)/vec.clear()/vec.size()/vec.capacity()
 vec.resize(new_size)改变大小/vec.reserve(capacity)预分配/vec.swap(other_vector)交换两个vector的内容，实际只交换指针和状态信息/vec.assign(n, value)重置，可以用于初始化/std::find(vec.begin(), vec.end(), value)查找value是否在vector中。如果找到，返回该元素的迭代器，否则返回vec.end()
 vec.begin()/end()返回迭代器
 vec == other_vector
 vec = other_vector
+
+//队列(queue)
+类似于栈，不过是先入先出//首端删除与尾端插入
+以std::deque/std::list为底层容器
+
+//双端队列(deque)
+栈+队列
+支持首端插入删除和尾端插入删除//普通数组可模拟
+可以通过[]访问元素(类似于vector 是普通通用容器 而非只暴露部分接口的容器适配器)
